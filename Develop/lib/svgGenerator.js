26c9d0c9aa84ce4a4f4ec08d8e1a9699c7e1ade2
JesusJr <Jesusujr101@gmail.com>
2023-11-08 21:23:35 -0500

    uploaded wrong code

--- Develop/lib/svgGenerator.js
+++ Develop/lib/svgGenerator.js
@@ -9,14 +9,14 @@ function svgGenerator(data) {
 
     const squareSvg = `
         <svg width="400" height="180" xmlns="http://www.w3.org/2000/svg">
-            <rect x="50" y="20" width="400" height="180" fill="blue"/>
-            <text x="50%" y="50%" fill="black" text-anchor="middle" font-size="55px" dominant-baseline="central">Square</text>
+            <rect x="50" y="20" width="400" height="180" fill="${data['shape-color']}"/>
+            <text x="50%" y="50%" fill="${data['text-color']}" text-anchor="middle" font-size="55px" dominant-baseline="central">${data.letters}</text>
         </svg>`;
 
     const triangleSvg = `
-        <svg height="250" width="500">
-            <polygon points="220,20 300,230 123,234" style="fill:lime;stroke:purple;stroke-width:1" />
-            <text x="220" y="130" fill="black" text-anchor="middle" font-size="30px" dominant-baseline="text-before-edge">Triangle</text>
+        <svg height="250" width="500" xmlns="http://www.w3.org/2000/svg">>
+            <polygon points="220,20 300,230 123,234" style="fill:${data['shape-color']};stroke:purple;stroke-width:1" />
+            <text x="220" y="130" fill="${data['text-color']}" text-anchor="middle" font-size="30px" dominant-baseline="text-before-edge">${data.letters}</text>
         </svg>`;
 
     if (data.shape.includes('Square')) {
@@ -33,6 +33,4 @@ function svgGenerator(data) {
 }
 
 module.exports = svgGenerator;
-
-
 
